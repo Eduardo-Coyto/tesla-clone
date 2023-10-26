8ba453afe4adbc7d169990d3fb78706daa1a8a94
Eduardo Coyto <eduardocoyto@gmail.com>
2023-10-25 22:32:54 -0300

    agrego icon tesla

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,17 +1,18 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: 'Tesla-Clone',
-  description: 'Aplicación de clon de la web de Tesla',
-}
+  title: "Tesla-Clone",
+  description: "Aplicación de clon de la web de Tesla",
+  icon: "https://www.tesla.com/themes/custom/tesla_frontend/assets/favicons/apple-touch-icon-114x114.png"
+};
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en" className='scroll-smooth'>
+    <html lang="en" className="scroll-smooth">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
