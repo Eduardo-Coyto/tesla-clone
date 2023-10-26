22d9eb9cda6623741900c2844382071217096f70
Eduardo Coyto <eduardocoyto@gmail.com>
2023-10-25 22:38:52 -0300

    agrego icon tesla2

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,7 +6,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Tesla-Clone",
   description: "Aplicación de clon de la web de Tesla",
-  icon: "https://www.tesla.com/themes/custom/tesla_frontend/assets/favicons/apple-touch-icon-114x114.png"
+  icons: {
+    icon: "https://www.tesla.com/themes/custom/tesla_frontend/assets/favicons/apple-touch-icon-114x114.png"
+  },
 };
 
 export default function RootLayout({ children }) {
